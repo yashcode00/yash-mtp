51b22a1096c57f3f3d8ae69e388488a097df83e5
Yash Sharma <yash.sharma@nirmata.com>
2024-02-18 13:38:10 +0530

    added rttm

---
+++ src/common/breakViaRTTM.py
@@ -0,0 +1,44 @@
+import os
+from pydub import AudioSegment
+
+# Function to parse RTTM file
+def parse_rttm(rttm_file):
+    segments = []
+    with open(rttm_file, 'r') as f:
+        for line in f:
+            parts = line.strip().split()
+            if len(parts) >= 4:
+                segments.append((float(parts[3]), float(parts[4]), parts[7]))
+    return segments
+
+# Function to split audio based on segments
+def split_audio(audio_file, segments, output_dir):
+    audio = AudioSegment.from_wav(audio_file)
+    for start, duration, lang in segments:
+        lang_dir = os.path.join(output_dir, "eng" if lang=="L1" else "not-eng")
+        os.makedirs(lang_dir, exist_ok=True)
+        segment_audio = audio[int(start * 1000):int((start + duration) * 1000)]
+        segment_audio.export(os.path.join(lang_dir, f"{os.path.basename(audio_file).split('.')[0]}_{start}_{duration}.wav"), format="wav")
+
+# Main function
+def main():
+    audio_dir = "/Users/yash/Downloads/audios"
+    rttm_dir = "/Users/yash/Downloads/rttm"
+    output_dir = "/Users/yash/Desktop/yash-mtp/datasets/displace-audioFromRTTM"
+
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    for audio_file in os.listdir(audio_dir):
+        if audio_file.endswith(".wav"):
+            base_name = audio_file.split(".")[0]
+            rttm_file = os.path.join(rttm_dir,base_name + "_LANGUAGE.rttm")
+            if os.path.isfile(rttm_file):
+                segments = parse_rttm(rttm_file)
+                audio_path = os.path.join(audio_dir, audio_file)
+                split_audio(audio_path, segments, output_dir)
+            else:
+                print(f"Unable to find rttm {rttm_file}")
+
+if __name__ == "__main__":
+    main()
